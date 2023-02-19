73ddbcd23c114812b147d8904a6e0bab3398bcb3
Влад Зелінський <zelinskijvlad5@gmail.com>
2023-02-19 17:04:36 +0200

    Fix thread synchronization errors

--- Service/Models/SaveJson.cs
+++ Service/Models/SaveJson.cs
@@ -6,31 +6,46 @@ namespace Service.Models
     public class SaveJson
     {
         private readonly FileData _fileData;
+        private object _locker;
 
         public SaveJson(FileData fileData)
         {
             _fileData = fileData;
+            _locker = new object();
         }
 
         public async Task SaveAsync(OutputTransaction output)
         {
-            string path = _fileData.PathB + "\\output" + GetCountFiles();
-
-            using (FileStream fs = new FileStream(path, FileMode.Create))
+            lock (_locker)
             {
-                var options = new JsonSerializerOptions
+                string pathDirectory = _fileData.PathB + "\\" + GetCurrentDirectory();
+
+                CheckFolder(pathDirectory);
+
+                string pathFile = pathDirectory + "\\output" + GetCountFiles(pathDirectory) + ".json";
+
+                using (FileStream fs = new FileStream(pathFile, FileMode.Create))
                 {
-                    WriteIndented = true,
-                };
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                    };
 
-                string json = JsonSerializer.Serialize(output, options);
+                    string json = JsonSerializer.Serialize(output, options);
 
-                byte[] input = Encoding.Default.GetBytes(json);
+                    byte[] input = Encoding.Default.GetBytes(json);
 
-                await fs.WriteAsync(input);
+                    fs.WriteAsync(input);
+                }
             }
         }
 
-        private int GetCountFiles() => Directory.GetFiles(_fileData.PathB).Length;        
+        private int GetCountFiles(string path) => Directory.GetFiles(path).Length;
+        private string GetCurrentDirectory() => DateTime.Now.ToString("MM-dd-yyyy");
+        private void CheckFolder(string pathFolder)
+        {
+            if (!Directory.Exists(pathFolder))
+                Directory.CreateDirectory(pathFolder);
+        }
     }
 }
