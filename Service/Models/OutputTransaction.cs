ad771229d7ec9c1ce01e88ee07e4a78411c54e83
Влад Зелінський <zelinskijvlad5@gmail.com>
2023-02-19 16:30:46 +0200

    Added method that processes input data

--- Service/Models/OutputTransaction.cs
+++ Service/Models/OutputTransaction.cs
@@ -3,6 +3,49 @@
     public class OutputTransaction
     {
         public Transaction[] Transactions { get; set; }
+
+        public static OutputTransaction Transform(List<InputTransaction> transactions)
+        {
+            List<Transaction> result = new List<Transaction>();
+            List<Service> listServices = new List<Service>();
+            List<Payer> listPayers = new List<Payer>();
+
+            var groupCities = transactions.GroupBy(x => x.Address.Split(",")[0]);
+
+            foreach (var c in groupCities)
+            {
+                Transaction transaction = new();
+                listServices.Clear();
+                transaction.City = c.Key;
+                transaction.Total = c.Sum(x => x.Payment);
+
+                var groupServices = c.GroupBy(x => x.Service);
+
+                foreach (var s in groupServices)
+                {
+                    Service service = new();
+                    listPayers.Clear();
+                    service.Name = s.Key;
+                    service.Total = s.Sum(x => x.Payment);
+
+                    foreach (var payer in s)
+                        listPayers.Add(new Payer()
+                        {
+                            Name = payer.FirstName + " " + payer.LastName,
+                            Paymant = payer.Payment,
+                            Date = payer.Date,
+                            AccountNumber = payer.AccountNumber
+                        });
+
+                    service.Payers = listPayers.ToArray();
+                    listServices.Add(service);
+                }
+                transaction.Services = listServices.ToArray();
+                result.Add(transaction);
+            }
+
+            return new OutputTransaction() { Transactions = result.ToArray() };
+        }
     }
 
     public class Transaction
