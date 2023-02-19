9c049da6a8df9400f166c6da5916feeffbba7e26
Влад Зелінський <zelinskijvlad5@gmail.com>
2023-02-19 16:13:44 +0200

    Added functionality to save JSON files

---
+++ Service/Models/SaveJson.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace Service.Models
+{
+    public class SaveJson
+    {
+        private readonly FileData _fileData;
+
+        public SaveJson(FileData fileData)
+        {
+            _fileData = fileData;
+        }
+
+        public async Task SaveAsync(OutputTransaction output)
+        {
+            string path = _fileData.PathB + "\\output" + GetCountFiles();
+
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                };
+
+                string json = JsonSerializer.Serialize(output, options);
+
+                byte[] input = Encoding.Default.GetBytes(json);
+
+                await fs.WriteAsync(input);
+            }
+        }
+
+        private int GetCountFiles() => Directory.GetFiles(_fileData.PathB).Length;        
+    }
+}
