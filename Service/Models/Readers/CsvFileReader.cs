f18fbcf2995b60ef8eb2cce3365f9eb8bbff9e99
Влад Зелінський <zelinskijvlad5@gmail.com>
2023-02-27 11:23:16 +0200

    Refactoring classes CsvFileReader and TextFileReader

--- Service/Models/Readers/CsvFileReader.cs
+++ Service/Models/Readers/CsvFileReader.cs
@@ -1,57 +1,14 @@
 ﻿namespace Service.Models.Readers
 {
-    public class CsvFileReader : IFileReader
+    public class CsvFileReader : FileReaderBase
     {
-        private readonly ILogger _logger;
-        private readonly string _filePath;
-
-        public CsvFileReader(string filePath)
+        public CsvFileReader(string filePath) : base(filePath, "CsvFileReader")
         {
-            _filePath = filePath;
-            _logger = Core.GetLogger("CsvFileReader");
         }
 
-        public async Task<ReadingResult> ReadFileAsync()
+        protected override bool ShouldSkipLine(string line)
         {
-            List<InputTransaction> result = new List<InputTransaction>();
-
-            int invalidLines = 0;
-            int currentLine = 0;
-
-            if (File.Exists(_filePath))
-            {
-                using (StreamReader sr = new StreamReader(_filePath))
-                {
-                    string? line;
-
-                    _logger.LogInformation($"The Service reading: {_filePath}");
-
-                    while ((line = await sr.ReadLineAsync()) != null)
-                    {
-                        if (line.Contains("first"))
-                            continue;
-
-                        currentLine++;
-
-                        var transaction = InputTransaction.GetTransaction(line);
-
-                        if (transaction != null)
-                            result.Add(transaction);
-                        else
-                            invalidLines++;
-                    }
-
-                    _logger.LogInformation($"The service stopped reading: {_filePath}");
-                }
-            }
-
-            return new ReadingResult
-            {
-                Transactions = result,
-                InvalidLines = invalidLines,
-                Lines = currentLine,
-                NameFile = _filePath
-            };
+            return line.Contains("first") || line.Contains("address");
         }
     }
 }
---
+++ Service/Models/Readers/FileReaderBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Models.Readers
+{
+    public abstract class FileReaderBase : IFileReader
+    {
+        protected readonly ILogger _logger;
+        protected readonly string _filePath;
+
+        protected FileReaderBase(string filePath, string loggerName)
+        {
+            _filePath = filePath;
+            _logger = Core.GetLogger(loggerName);
+        }
+
+        public async Task<ReadingResult> ReadFileAsync()
+        {
+            List<InputTransaction> result = new List<InputTransaction>();
+
+            int invalidLines = 0;
+            int currentLine = 0;
+
+            if (File.Exists(_filePath))
+            {
+                using (StreamReader sr = new StreamReader(_filePath))
+                {
+                    string? line;
+
+                    _logger.LogInformation($"The Service reading: {_filePath}");
+
+                    while ((line = await sr.ReadLineAsync()) != null)
+                    {
+                        if (!ShouldSkipLine(line))
+                        {
+                            currentLine++;
+                            var transaction = InputTransaction.GetTransaction(line);
+
+                            if (transaction != null)
+                                result.Add(transaction);
+                            else
+                                invalidLines++;
+                        }
+                    }
+
+                    _logger.LogInformation($"The service stopped reading: {_filePath}");
+                }
+            }
+
+            return new ReadingResult
+            {
+                Transactions = result,
+                InvalidLines = invalidLines,
+                Lines = currentLine,
+                NameFile = _filePath
+            };
+        }
+
+        protected abstract bool ShouldSkipLine(string line);
+    }
+}
